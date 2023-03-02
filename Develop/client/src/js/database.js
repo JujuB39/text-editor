2e5dfe5b04570a49ce4db77ff5a38dad953048fe
Julissa Bonilla <julissab9@gmail.com>
2023-03-01 22:55:08 -0600

    working on js

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,9 +13,27 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const jateDb = await openDB('jate', 1);
+  const get = jateDb.transaction("jate", "readwrite")
+  const storeDb = get.objectStore('jate')
+  const request = storeDb.put({id: 1, value: content})
+  const result = await request;
+  console.log(result)
+  
+  // console.error('putDb not implemented');
+}
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const jateDb = await openDB('jate', 1);
+  const get = jateDb.transaction("jate", "readonly")
+  const storeDb = get.objectStore('jate')
+  const request = storeDb.getAll()
+  const result = await request;
+  return result; 
+ 
+
+}
 
 initdb();
